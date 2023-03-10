670a494163f7f1e04e110c745ba6cc55b9697089
boni98 <bonizhang2@gmail.com>
2023-03-09 19:47:17 -0500

    update sound animation

--- hwk05/main.js
+++ hwk05/main.js
@@ -16,14 +16,26 @@ function audioToggle(msg) {
 // TOGGLE ON/OFF sound button animation
 var svg = document.getElementById("sound");
 
+var isPlaying = true
 const circleWrapObj = document.querySelector(".btn");
 const myC2ID = document.querySelector("#c2id");
 const myC1ID = document.querySelector("#c1id");
 
 circleWrapObj.addEventListener("click", () => {
-	myC2ID.classList.toggle("circle2");
-	myC1ID.classList.toggle("circle1");
-
+	if(isPlaying){
+		circleWrapObj.classList.remove("pause")
+		circleWrapObj.classList.add("play")
+		myC2ID.style.animationPlayState = "running";
+   	 	myC1ID.style.animationPlayState = "running";
+		isPlaying=false;
+	}
+	else{
+		circleWrapObj.classList.remove("play")
+		circleWrapObj.classList.add("pause")
+		myC2ID.style.animationPlayState = "paused";
+    	myC1ID.style.animationPlayState = "paused";
+		isPlaying=true;
+	}
 });
 
 // HOVER ON/OFF hat svg with ticket text
